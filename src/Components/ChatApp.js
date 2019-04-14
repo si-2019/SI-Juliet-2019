02f8674b6cd9201dddbe41f0b94f54dd8f24ee15
Amar Luckin <aluckin3@etf.unsa.ba>
2019-04-14 18:40:50 +0200

    Chat bot added.

--- src/Components/ChatApp.js
+++ src/Components/ChatApp.js
@@ -6,11 +6,24 @@ import {instanceLocator, testToken, roomId} from './../config.js'
 import UsersList from './UsersList';
 import '../styles/ChatApp.css'
 
+let predmeti = require('../predmeti.json');
+function findPredmetId(nameOfPredmet) {
+    for(var i = 0; i < Object.keys(predmeti).length; i++) {
+        for(var j = 0; j < Object.keys(predmeti[i].name).length; j++) {
+            if(predmeti[i].name[j].toLowerCase() === nameOfPredmet.toLowerCase()) {
+                return predmeti[i].id;   
+            }
+        }
+    }
+    return -1; 
+}
+
 class ChatApp extends Component {
     constructor(props) {
         super(props);
         this.state = {
             currentUser: null,
+            boUser: null,
             currentRoom: { users: [] },
             messages: [],
             users: []
@@ -27,11 +40,24 @@ class ChatApp extends Component {
             })
         })
 
+        const chatManagerBot = new ChatManager({
+            instanceLocator: instanceLocator,
+            userId: "bot@autoreply",
+            tokenProvider: new TokenProvider({
+                url: testToken
+            })
+        })
+        
+        chatManagerBot.connect().then(currentUser => {
+            this.setState({botUser: currentUser})
+            console.log(currentUser);
+            return currentUser;
+        })
+
         chatManager
             .connect()
             .then(currentUser => {
                 this.setState({ currentUser: currentUser })
-
                 return currentUser.subscribeToRoom({
                     roomId: roomId,
                     messageLimit: 100,
@@ -51,7 +77,7 @@ class ChatApp extends Component {
                 this.setState({
                     currentRoom,
                     users: currentRoom.users
-                })
+                })                
             })
             .catch(error => console.log(error))
         }
@@ -60,10 +86,29 @@ class ChatApp extends Component {
     addMessage(text) {
         console.log(this.state);
         this.state.currentUser.sendMessage({
-            text,
+            text: text,
             roomId: this.state.currentRoom.id
+        }).then(() => {
+            if(text.substr(0, 11) === "@izvjestaj ") {
+                var name = text.substr(11);
+                var predmetGodina = name.split(", ");
+                var predmetId = findPredmetId(predmetGodina[0]);
+                if (predmetId != -1) {
+                    this.state.botUser.sendMessage({
+                        text: "https://zamger.etf.unsa.ba/?sta=izvjestaj/predmet&predmet=" + predmetId + "&ag=" + 
+                        (parseInt(predmetGodina[1]) - 2005) + "&sm_arhiva=0",
+                        roomId: this.state.currentRoom.id
+                    })
+                } else {
+                    this.state.botUser.sendMessage({
+                        text: "Nepostojeci predmet",
+                        roomId: this.state.currentRoom.id
+                    })
+                }
+            }
         })
-            .catch(error => console.error('error', error));
+          .catch(error => console.error('error', error));
+        
     }
 
     render() {
