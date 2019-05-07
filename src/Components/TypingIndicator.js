ad2874ee58d16a4e801cd8e9a7dc3bdb4d2ffa84
unknown <mnedic1@etf.unsa.ba>
2019-05-07 17:14:04 +0200

    Stil + chatapp still

--- src/Components/TypingIndicator.js
+++ src/Components/TypingIndicator.js
@@ -4,20 +4,19 @@ class TypingIndicator extends Component {
         if (this.props.typingUsers.length > 0) {
             return (
               <div style={divStyle}>
-                {'Typing: ' + `${this.props.typingUsers
-                  .slice(0, this.props.typingUsers.length)
-                  .join(', ')}`}
+                {'Typing: ' + this.props.typingUsers.join(', ') + '...' }
               </div>
             )
           }
-          return (
-            <div />
-          )
+        return (
+            <div> {''} </div>
+        )
     }
 }
 
 const divStyle = {
-    border: '1px solid blue'
+    border: '2px solid #5E0565',
+    marginTop: '0px'
 }
 
 export default TypingIndicator
