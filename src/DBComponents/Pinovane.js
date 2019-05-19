caa4c9adfb28cbe5dcf4591322e75aa4320a1318
rpedljak <rijad.pedljak1@gmail.com>
2019-05-19 19:45:34 +0200

    messageid za pinovanu unique

--- src/DBComponents/Pinovane.js
+++ src/DBComponents/Pinovane.js
@@ -3,7 +3,10 @@ const Sequelize = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
     const pinovanePoruke = sequelize.define('PinovanePoruke', {
         messageCreatedAt: Sequelize.STRING, 
-        messageId: Sequelize.STRING,
+        messageId: {
+            type: Sequelize.STRING,
+            unique: true
+        }
         roomId: Sequelize.STRING, 
         senderId: Sequelize.STRING, 
         text: Sequelize.STRING
