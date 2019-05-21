2175b104c1f3b781c2eee8ee20c4ab080f1ced74
jpetrovic1 <jpetrovic1@etf.unsa.ba>
2019-05-21 03:33:21 +0200

    gotov user story za pridruživanje javnim grupama

--- src/Components/RoomList.js
+++ src/Components/RoomList.js
@@ -6,6 +6,7 @@ class RoomList extends Component {
         super(props);
     
         this.state = {
+            joinableRooms:this.props.joinableRooms,
             rooms: this.props.rooms
         }
     }
@@ -16,11 +17,20 @@ class RoomList extends Component {
                 <div style={{color: 'white' }}>
                 <h2 style={{marginTop: '5rem', marginBottom: '1rem'}}>Rooms</h2>
                 <ul style={{overflowY: 'scroll', overflowX: 'hidden', height:'400px'}}>
-                        <h4>Public rooms</h4>
+                <h4>Joined Public Rooms</h4>
                         {this.props.rooms.filter(room => !room.isPrivate).map((room, index) => {
                             const active = this.props.room.id === room.id ? "active" : "";
                             return <li className={"room" + active} onClick={() => this.props.joinRoomById(room.id)}
-                            key={index}><h4> #{room.name}</h4> </li>
+                            key={index}><h5> #{room.name}</h5> </li>
+                        })}
+                        <br />
+                        <br />
+                        <h4>Joinable Public Rooms</h4>
+                        {this.props.joinableRooms.map(room => {
+                        return (
+                            <li key={room.id} className="room" onClick={()=>this.props.joinRoomById(room.id)}>
+                                <h5> #{room.name}</h5> </li>                        
+                            )
                         })}
                         <br />
                         <br />
