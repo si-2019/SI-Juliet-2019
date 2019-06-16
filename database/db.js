aa41c935473e87255f5448d5c7cf134190ff38ad
unknown <mnedic1@etf.unsa.ba>
2019-06-16 15:53:51 +0200

    Promjena baze za ispravku brisanja fajlova

--- database/db.js
+++ database/db.js
@@ -2,9 +2,14 @@ const Sequelize = require('sequelize');
 const path = require('path')
 
 const sequelize = new Sequelize(
+    /*
     "TYQcLL35gV",   // database
     "TYQcLL35gV",   // username
     "BLysSj9ZrP",   // password
+    */
+   "cHVC4EIVNS",
+   "cHVC4EIVNS",
+   "AzreiKIy8F"
     {
         host:"37.59.55.185",
         dialect:"mysql"
